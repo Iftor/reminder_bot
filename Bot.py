4d45188aec00709006f9b912865e513c8b2d7dc3
Кирилл Архипов <arkhipovkirill2001@gmail.com>
2021-03-26 20:13:17 +0300

    Edited start command, added buttons

--- Bot.py
+++ Bot.py
@@ -11,4 +11,9 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Привет, я бот напоминатель')
+    markup = telebot.types.ReplyKeyboardMarkup(row_width=1)
+    edit_button = telebot.types.KeyboardButton('Редактировать напоминание')
+    create_button = telebot.types.KeyboardButton('Создать напоминание')
+    all_list_button = telebot.types.KeyboardButton('Список всех напоминаний')
+    markup.add(edit_button, create_button, all_list_button)
+    bot.send_message(chat_id=message.chat.id, text="Выберете действие:", reply_markup=markup)
