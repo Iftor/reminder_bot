fc7d31c18401e03ce6952ab08b8599b7867c94ab
Кирилл Архипов <arkhipovkirill2001@gmail.com>
2021-03-26 20:34:52 +0300

    Added handler to messages without logic

--- Bot.py
+++ Bot.py
@@ -17,3 +17,15 @@ def start_message(message):
     all_list_button = telebot.types.KeyboardButton('Список всех напоминаний')
     markup.add(edit_button, create_button, all_list_button)
     bot.send_message(chat_id=message.chat.id, text="Выберете действие:", reply_markup=markup)
+
+
+@bot.message_handler(content_types=['text'])
+def handle_message(message):
+    if message.text == 'Редактировать напоминание':
+        bot.send_message(message.chat.id, 'Редактируем')
+    elif message.text == 'Создать напоминание':
+        bot.send_message(message.chat.id, 'Создаем')
+    elif message.text == 'Список всех напоминаний':
+        bot.send_message(message.chat.id, 'Список')
+    else:
+        bot.send_message(message.chat.id, 'Не понимаю, о чем ты')
