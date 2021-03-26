7501bdc87f6d9cdf3d8541d946c70e38fa146281
Кирилл Архипов <arkhipovkirill2001@gmail.com>
2021-03-26 19:25:26 +0300

    Added start command to bot

--- Bot.py
+++ Bot.py
@@ -1,1 +1,14 @@
 import telebot
+
+try:
+    from Token import token
+except ImportError:
+    token = ''
+
+
+bot = telebot.TeleBot(token)
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет, я бот напоминатель')
