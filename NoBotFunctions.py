fafaf69a74914264de88a6fc9e0fe387eaf638e0
Кирилл Архипов <arkhipovkirill2001@gmail.com>
2021-03-31 01:00:22 +0300

    Added issue remind function

--- NoBotFunctions.py
+++ NoBotFunctions.py
@@ -11,3 +11,17 @@ def add_remind(date_rem_id):
     remind = {'date': date, 'text': date_rem_id[1], 'id': date_rem_id[2]}
     reminds.append(remind)
     date_rem_id[0], date_rem_id[1], date_rem_id[2] = None, None, None
+
+
+def issue_remind():
+    while True:
+        current_datetime = datetime.now()
+        for i, remind in enumerate(reminds):
+            if (remind['date'].day == current_datetime.day
+                    and remind['date'].month == current_datetime.month
+                    and remind['date'].hour == current_datetime.hour
+                    and remind['date'].minute == current_datetime.minute):
+                bot.send_message(remind['id'], remind['text'])
+                reminds.pop(i)
+        else:
+            sleep(30)
