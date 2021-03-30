4b38d2835f18686344e7d8f0dafcc761ae630627
Кирилл Архипов <arkhipovkirill2001@gmail.com>
2021-03-31 00:59:19 +0300

    Changed write reminds function

--- NoBotFunctions.py
+++ NoBotFunctions.py
@@ -1,13 +1,13 @@
+from datetime import datetime
+from Bot import bot
+from time import sleep
+
+
 reminds = []
 
 
-def add_remind(date_rem: list[str]):
-    date = date_rem[0]
-    remind = date_rem[1]
-    date, time = date.split(' ')
-    day, month = date.split('.')
-    hour, minute = time.split(':')
-    reminds.append({'day': day, 'month': month, 'hour': hour, 'minute': minute})
-    
-    
-    
+def add_remind(date_rem_id):
+    date = datetime.strptime(date_rem_id[0], '%d.%m %H:%M')
+    remind = {'date': date, 'text': date_rem_id[1], 'id': date_rem_id[2]}
+    reminds.append(remind)
+    date_rem_id[0], date_rem_id[1], date_rem_id[2] = None, None, None
