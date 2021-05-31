8c4956ed1e20f85c119398e1504da324bbb04a07
Кирилл Архипов <arkhipovkirill2001@gmail.com>
2021-06-01 01:26:49 +0300

    Added viewing list of reminders function

--- NoBotFunctions.py
+++ NoBotFunctions.py
@@ -2,11 +2,14 @@ from Bot import bot
 from time import sleep
 import schedule
 
+reminds = dict()
+
 
 def send_message_func(remind):
     """Функция выдачи напоминания"""
     
     bot.send_message(remind['id'], remind['text'])
+    reminds[remind['id']].remove(remind)
     return schedule.CancelJob
 
 
@@ -14,9 +17,23 @@ def add_remind(time_rem_id):
     """Функция добавления напоминания в расписание"""
     
     time = time_rem_id[0].split(':')
-    remind = {'text': time_rem_id[1], 'id': time_rem_id[2]}
+    remind = {'time': time_rem_id[0], 'text': time_rem_id[1], 'id': time_rem_id[2]}
     schedule.every().day.at(f'{time[0]}:{time[1]}').do(send_message_func, remind=remind)
+    if remind['id'] in reminds:
+        reminds[remind['id']].append(remind)
+    else:
+        reminds[remind['id']] = [remind]
     time_rem_id[0], time_rem_id[1], time_rem_id[2] = None, None, None
+
+
+def print_reminds_list(id):
+    message = ''
+    if id in reminds:
+        for remind in reminds[id]:
+            message += f'{remind["time"]}: {remind["text"]}\n'
+    else:
+        message = 'Список пуст'
+    bot.send_message(id, message)
 
 
 def issue_remind():
